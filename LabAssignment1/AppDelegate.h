665452d8be0dea1d3c3160ecdb37dd25dce364d3
Mannam <pmq62@kc-sce463lab-15.kc.umkc.edu>
2015-06-12 15:11:57 -0500

    Initial Commit

---
+++ LabAssignment1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LabAssignment1
+//
+//  Created by Mannam, Prithvi Krishna (UMKC-Student) on 6/12/15.
+//  Copyright (c) 2015 Mannam, Prithvi Krishna (UMKC-Student). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LabAssignment1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LabAssignment1
+//
+//  Created by Mannam, Prithvi Krishna (UMKC-Student) on 6/12/15.
+//  Copyright (c) 2015 Mannam, Prithvi Krishna (UMKC-Student). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
